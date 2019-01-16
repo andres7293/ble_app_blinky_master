2ffd45d0eb9918fc34fa4c3221b38ae40a71e948
andres <andres7293@gmail.com>
2019-01-16 19:55:33 +0000

    Enciende el led propio y el del esclavo cuando encuentra al esclavo via ble

---
+++ pca10040/s132/armgcc/_build/nrf52832_xxaa/ble_srv_common.c.d
@@ -0,0 +1,191 @@
+_build/nrf52832_xxaa/ble_srv_common.c.o: \
+ ../../../../../../components/ble/common/ble_srv_common.c \
+ ../../../../../../components/ble/common/ble_srv_common.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stdint.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/stdint.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/machine/_default_types.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/features.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/_newlib_version.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/_intsup.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/_stdint.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stdbool.h \
+ ../../../../../../components/softdevice/s132/headers/ble_types.h \
+ ../../../../../../components/libraries/util/app_util.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stddef.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_name_change.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../components/softdevice/s132/headers/ble.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_svc.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../../components/softdevice/s132/headers/ble_err.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gap.h \
+ ../../../../../../components/softdevice/s132/headers/ble_hci.h \
+ ../../../../../../components/softdevice/s132/headers/ble_ranges.h \
+ ../../../../../../components/softdevice/s132/headers/ble_types.h \
+ ../../../../../../components/softdevice/s132/headers/ble_l2cap.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gatt.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gattc.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gatts.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gap.h \
+ ../../../../../../components/softdevice/s132/headers/ble_gatt.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/string.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/_ansi.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/newlib.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/config.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/machine/ieeefp.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/reent.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/_ansi.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/_types.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/machine/_types.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/lock.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/cdefs.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/string.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/libraries/util/app_error.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/stdio.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stdarg.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/types.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/machine/types.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/stdio.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../../components/libraries/util/app_error_weak.h
+
+../../../../../../components/ble/common/ble_srv_common.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stdint.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/stdint.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/machine/_default_types.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/features.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/_newlib_version.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/_intsup.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/_stdint.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stdbool.h:
+
+../../../../../../components/softdevice/s132/headers/ble_types.h:
+
+../../../../../../components/libraries/util/app_util.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stddef.h:
+
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../modules/nrfx/mdk/nrf.h:
+
+../../../../../../modules/nrfx/mdk/nrf52.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../../../modules/nrfx/mdk/system_nrf52.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_bitfields.h:
+
+../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_name_change.h:
+
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../../components/softdevice/s132/headers/ble.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_svc.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error.h:
+
+../../../../../../components/softdevice/s132/headers/ble_err.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gap.h:
+
+../../../../../../components/softdevice/s132/headers/ble_hci.h:
+
+../../../../../../components/softdevice/s132/headers/ble_ranges.h:
+
+../../../../../../components/softdevice/s132/headers/ble_types.h:
+
+../../../../../../components/softdevice/s132/headers/ble_l2cap.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gatt.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gattc.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gatts.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gap.h:
+
+../../../../../../components/softdevice/s132/headers/ble_gatt.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/string.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/_ansi.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/newlib.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/config.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/machine/ieeefp.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/reent.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/_ansi.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/_types.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/machine/_types.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/lock.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/cdefs.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/string.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/libraries/util/app_error.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/stdio.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stdarg.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/types.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/machine/types.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/stdio.h:
+
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error.h:
+
+../../../../../../components/libraries/util/app_error_weak.h:
---
+++ pca10040/s132/armgcc/_build/nrf52832_xxaa/utf.c.d
@@ -0,0 +1,29 @@
+_build/nrf52832_xxaa/utf.c.o: \
+ ../../../../../../external/utf_converter/utf.c \
+ ../../../../../../external/utf_converter/utf.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stddef.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stdint.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/stdint.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/machine/_default_types.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/features.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/_newlib_version.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/_intsup.h \
+ /home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/_stdint.h
+
+../../../../../../external/utf_converter/utf.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stddef.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stdint.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/stdint.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/machine/_default_types.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/features.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/_newlib_version.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/_intsup.h:
+
+/home/andres/Documentos/nordic-toolchain/gcc-arm-none-eabi-8-2018-q4-major/arm-none-eabi/include/sys/_stdint.h:
